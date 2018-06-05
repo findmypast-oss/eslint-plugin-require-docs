614901928ce5ff6885859b823135b0ca36604335
Emily McDonald <mcdonald.emily@live.co.uk>
2018-06-05 15:15:55 +0100

    :white_check_mark: Added tests for expected flow-types behaviour

---
+++ tests/lib/rules/flow-types.js
@@ -0,0 +1,52 @@
+var rule = require("../../../lib/rules/flow-types"),
+  RuleTester = require("eslint").RuleTester;
+
+var ruleTester = new RuleTester();
+ruleTester.run("types", rule, {
+  valid: [
+    {
+      code: `type Props = {
+        /** JSDoc comment for prop abc */
+        abc: string, 
+        /** JSDoc comment for prop def */
+        def: number
+    }`,
+      options: [{ docType: "jsdoc" }],
+      parser: "babel-eslint"
+    }
+  ],
+
+  invalid: [
+    {
+      code: `type Props = {
+            abc: string,
+            def: number
+        }`,
+      options: [{ docType: "jsdoc" }],
+      parser: "babel-eslint",
+      errors: [
+        {
+          message: "You must provide a jsdoc comment for property 'abc'"
+        },
+        {
+          message: "You must provide a jsdoc comment for property 'def'"
+        }
+      ]
+    },
+    {
+      code: `type Props = {
+            // Non-JSDoc comment for prop abc
+            abc: string,
+            /** JSDoc comment for prop def */
+            def: number
+        }`,
+      options: [{ docType: "jsdoc" }],
+      parser: "babel-eslint",
+      errors: [
+        {
+          message: "You must provide a jsdoc comment for property 'abc'"
+        }
+      ]
+    }
+  ]
+});
